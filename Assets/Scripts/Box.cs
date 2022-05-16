d41c0dd81f701366f81de89d6da0cb913d5aa0a7
Jean Ward <Jeanward005@gmail.com>
2022-05-16 19:18:23 -0400

    Initial Comit and Ignore file completed

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public int index;
+    public Mark mark;
+    public bool isMarked;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        index = transform.GetSiblingIndex();
+        mark = Mark.None;
+
+        isMarked = false;
+
+    }
+
+    public void SetAsMarked( Sprite sprite, Mark mark, Color color)
+    {
+        isMarked = true;
+        this.mark = mark;
+
+        spriteRenderer.color = color;
+        spriteRenderer.sprite = sprite;
+
+        // disable the circle collifer2d to avoid marking it twice 
+        GetComponent<CircleCollider2D>().enabled = false;
+    }
+
+}
